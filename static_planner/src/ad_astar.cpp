7235bcf4234b21eb471ebe8210b82c9ea9b37c44
wz_hust <wangzhao9562@gmail.com>
2019-12-16 18:17:49 +0800

    Add static_planner source file

--- static_planner/src/ad_astar.cpp
+++ static_planner/src/ad_astar.cpp
@@ -15,126 +15,98 @@ AdAStarPlanner::AdAStarPlanner(int xs, int ys, double resolution) :
 
 bool AdAStarPlanner::getPlan(unsigned char* costs, double start_x, double start_y, double start_th, double end_x, double end_y, double end_th, int cycles, std::vector< std::pair<float, float> >& plan)
 {
-        ROS_INFO("try to search path in planner, heading: %f", start_th);
-        ROS_INFO("start in map: %f, %f; end in map: %f, %f", start_x, start_y, end_x, end_y);
-  
         double start_x_up = std::ceil(start_x), start_y_up = std::ceil(start_y);
         double end_x_up = std::ceil(end_x), end_y_up = std::ceil(end_y);
 
 	wrapper_.initialize(costs, toIndex(start_x_up, start_y_up), lethal_cost_, nx_, ny_, rough_lenth_, resolution_, unknown_);
-        ROS_INFO("initialization of wrapper finish");
-        wrapper_.printNewCosts();
-        wrapper_.printOriCosts();
-        ROS_INFO("lowerleft of new costmap: %d", wrapper_.getLowerLeft());
-        ROS_INFO("lowerleft of new costmap index: %d, %d", wrapper_.getLowerLeftX(), wrapper_.getLowerLeftY());
-        ROS_INFO("rough len of new cosmtap: %d, %f, %f", wrapper_.getRoughLen(), wrapper_.getRoughLenInWorld(), wrapper_.getOriMapResolution());
-        ROS_INFO("size of new map: %d, %d", wrapper_.getNewMapWidth(), wrapper_.getNewMapHeight());
+        // wrapper_.printNewCosts();
+        // wrapper_.printOriCosts();
 	
 	std::pair<int, int> start_wrapper = wrapper_.getCoordInNewCostmap(toIndex(start_x_up, start_y_up));
 	double start_x_wrapper = start_wrapper.first;
 	double start_y_wrapper = start_wrapper.second;
-        ROS_INFO("start in new costmap: %f, %f", start_x_wrapper, start_y_wrapper);
 	
 	std::pair<int, int> end_wrapper = wrapper_.getCoordInNewCostmap(toIndex(end_x_up, end_y_up));
 	double end_x_wrapper = end_wrapper.first;
 	double end_y_wrapper = end_wrapper.second;
-        ROS_INFO("end in new costmap: %f, %f", end_x_wrapper, end_y_wrapper);
 	
 	int* new_costs = wrapper_.getNewCosts();
 	
 	// if(searchPath(costs, start_x, start_y, start_th, end_x, end_y, end_th, cycles)){
 	if(searchPath(new_costs, start_x_wrapper, start_y_wrapper, start_th, end_x_wrapper, end_y_wrapper, end_th, wrapper_.getNewMapWidth() * wrapper_.getNewMapHeight() * 2)){
-            ROS_INFO("try to get path from goal recursively");
+            // ROS_INFO("try to get path from goal recursively");
        
 	    Index* temp = end_index_.p_;
             plan.push_back(std::pair<float, float>(end_x, end_y));
             
 	    while(temp->p_ != nullptr){
-			// int x = temp->i_ % nx_, y = temp->i_ / nx_;
-			int ns = wrapper_.getIndexInCostmap(temp->i_);
-			int x = ns % nx_, y = ns / nx_;
-            // ROS_INFO("push (%d, %d) into path", x, y);
+		// int x = temp->i_ % nx_, y = temp->i_ / nx_;
+		int ns = wrapper_.getIndexInCostmap(temp->i_);
+		int x = ns % nx_, y = ns / nx_;
+                // ROS_INFO("push (%d, %d) into path", x, y);
 	    	plan.push_back(std::pair<float, float>((float)x, (float)y));
                 
-               std::string filename = "/home/wz9562/Documents/path.txt";
-               std::fstream fs(filename, std::ios_base::app);
-               if(!fs.is_open()){
-                    ROS_INFO("wrong to open file to record path");
-               } 
-               else{
-                    fs << x << "," << y << " ";
-               }
-               fs.close();
-
-            temp = temp->p_;
+                temp = temp->p_;
 	    }
-            ROS_INFO("length of path is: %d", plan.size());
+            plan.push_back(std::pair<float, float>(start_x, start_y)); // add start point
 	    return true;
 	}
 	else
-		return false;
+	    return false;
 }
 
 
 bool AdAStarPlanner::searchPath(int* costs, double start_x, double start_y, double start_th, double end_x, double end_y, double end_th, int cycles){
-    ROS_INFO("start to search path");	
+    // ROS_INFO("start to search path");	
 	
     total_distance_ = hypot(end_x - start_x, end_y - start_y);										
-	queue_.clear();
+    queue_.clear();
 
-    // ROS_INFO("filter the orientation");
+    // Cal start position
     int start_th_f = orientationFilter(start_th); // get start th after filtering
-    // ROS_INFO("get next pos");
     std::pair<int, int> next_pos = nextPos(start_th_f); // get next Index along the orientation
-   
     int start_i = wrapper_.toIndex(static_cast<int>(start_x) + next_pos.first, static_cast<int>(start_y) + next_pos.second);
-	
     // check if next pos is reachable
-    ROS_INFO("check if next pos is reachable: %d, %d", (int)start_x + next_pos.first, (int)start_y + next_pos.second);
-	if(costs[start_i] == 1){
-                ROS_INFO("next pos is unreachable");
-		return false;
-        }
-	
-    // ROS_INFO("prepare to start search");
-	queue_.push_back(Index(start_i, 0, start_th_f));
+    if(costs[start_i] == 1){
+      ROS_ERROR("static_planner: invalid start input");
+      return false;
+    }
+    queue_.push_back(Index(start_i, 0, start_th_f));
 
-    // ROS_INFO("current index is: %d", start_i);
+    // Cal goal position 
+    int end_th_f = orientationFilter(end_th);
+    std::pair<int, int> previous_pos = previousPos(end_th_f); 
+    // int goal_i = wrapper_.toIndex(end_x, end_y);
+    int goal_i = wrapper_.toIndex(static_cast<int>(end_x) + previous_pos.first, static_cast<int>(end_y) + previous_pos.second);
+    // check if last pos is reachable
+    if(costs[goal_i] == 1){
+      ROS_ERROR("static_planner: invalid goal input");
+      return false;
+    }    
 
-    // int goal_i = toIndex(end_x, end_y);
-    int goal_i = wrapper_.toIndex(end_x, end_y);
-	int cycle = 0;
+    // initialize counter
+    int cycle = 0;
 
     int new_nx_ = wrapper_.getNewMapWidth();
 
-    ROS_INFO("start search");
     while (queue_.size() > 0 && cycle < cycles) {
         Index top = queue_[0];
         std::pop_heap(queue_.begin(), queue_.end(), greater1()); // move minimum value to the end of container
 
         int i = top.i_;
         if (i == goal_i){
-			end_index_ = top;
-            ROS_INFO("finish searching for path, successfully");
-            return true;
-		}
+	   end_index_ = top;
+           return true;
+	}
 		
-		queue_.pop_back(); // pop the Index with minimum cost
+	queue_.pop_back(); // pop the Index with minimum cost
 		
 	// go through all neighbor of Index with minimum cost
-        // add(costs, top, i, i + 1, (int)end_x, (int)end_y);
-        // add(costs, top, i, i - 1, (int)end_x, (int)end_y);
-        // add(costs, top, i, i + new_nx_, (int)end_x, (int)end_y);
-        // add(costs, top, i, i - new_nx_, (int)end_x, (int)end_y);
-        // add(costs, top, i, i + new_nx_ - 1, (int)end_x, (int)end_y);
-        // add(costs, top, i, i + new_nx_ + 1, (int)end_x, (int)end_y);
-        // add(costs, top, i, i - new_nx_ - 1, (int)end_x, (int)end_y);
         traverse(costs, top, i, (int)end_x, (int)end_y);     
 
         cycle++;
     }
 
-    ROS_INFO("finish searching for path, failed");
     return false;
 }
 
@@ -218,40 +190,37 @@ void AdAStarPlanner::traverse(int* costs, Index min_fn_ind, int i, int end_x, in
 }
 
 void AdAStarPlanner::add(int* costs, Index min_fn_ind, int i, int next_i, int end_x,int end_y) {
-    // ROS_INFO("start check neighbor");
     int new_nx_ = wrapper_.getNewMapWidth();
 
     if (next_i < 0 || next_i >= wrapper_.getNewMapSize()){
-        // ROS_INFO("neighbor is out of bounds");
         return;
     }
 	
 	if(costs[next_i] == 1)
 		return;
     
-    // ROS_INFO("calc index orientation");
     int next_ind_th = CalcIndexOrientation(next_i - i, new_nx_);
 	int det_th = abs(min_fn_ind.th_ - next_ind_th); // calc the delta th between min_fn_ind and neighbor ind
 	if(det_th > 180)
 		det_th = 360 - det_th;
 	
-        // ROS_INFO("calc compensate");
 	float compensate = CalcCompensate(min_fn_ind.th_, det_th);
 	
     int x = next_i % new_nx_, y = next_i / new_nx_;
     // float distance = abs(end_x - x) + abs(end_y - y); // manhattan distance 
 	float distance = hypot(end_x - x, end_y - y); // euclidean distance
 	
-        // ROS_INFO("find neighbor in open list");
 	int pos_in_list = isInList(next_i);
 	
 	if(pos_in_list >= 0){
-		float gn = calcG(min_fn_ind, i, next_i - i);
-		float hn = calcH(compensate, distance);
-		float fn = gn + hn;
+		// float gn = calcG(min_fn_ind, i, next_i - i);
+	        float gn = calcG(min_fn_ind, i, next_i - i) + compensate;	 
+                // float hn = calcH(compensate, distance);
+		float hn = distance;
+                float fn = gn + hn;
 		if(fn < queue_[pos_in_list].fn_){
 			// ROS_INFO("change property of neighbor");
-            queue_[pos_in_list].gn_ = gn;
+                        queue_[pos_in_list].gn_ = gn;
 			queue_[pos_in_list].fn_ = fn;
 			queue_[pos_in_list].p_= new Index(min_fn_ind.i_, min_fn_ind.fn_, min_fn_ind.gn_, min_fn_ind.th_, min_fn_ind.p_);
 			queue_[pos_in_list].th_ = next_ind_th;
@@ -259,11 +228,13 @@ void AdAStarPlanner::add(int* costs, Index min_fn_ind, int i, int next_i, int en
 	}
 	else{
 		// ROS_INFO("neighbor is not in list, add it into list");
-		float gn = calcG(min_fn_ind, i, next_i - i);
-		float hn = calcH(compensate, distance);
-		float fn = gn + hn;
+		// float gn = calcG(min_fn_ind, i, next_i - i);
+	        float gn = calcG(min_fn_ind, i, next_i - i) + compensate;	 
+          	// float hn = calcH(compensate, distance);
+		float hn = distance;
+                float fn = gn + hn;
 		queue_.push_back(Index(next_i, fn, gn, next_ind_th, new Index(min_fn_ind.i_, min_fn_ind.fn_, min_fn_ind.gn_, min_fn_ind.th_, min_fn_ind.p_)));
-		std::push_heap(queue_.begin(), queue_.end(), greater1());
+		std::make_heap(queue_.begin(), queue_.end(), greater1());
 	}
 
     // queue_.push_back(Index(next_i, potential[next_i] + distance * neutral_cost_));
@@ -272,12 +243,14 @@ void AdAStarPlanner::add(int* costs, Index min_fn_ind, int i, int next_i, int en
 
 
 float AdAStarPlanner::calcG(Index cur_ind, int ind, int det_ind)const{
-	float extra_g = abs(det_ind) == 1 ? u_cost1_ : u_cost2_;
+	float extra_g = abs(det_ind) == 1 ?  u_cost1_ : u_cost2_;
 	return extra_g + cur_ind.gn_; 
 }
 
 float AdAStarPlanner::calcH(float compensate, float distance){
-	return (compensate + distance / total_distance_) * 10 * neutral_cost_;
+	// return (compensate + distance / total_distance_) * 10 * neutral_cost_;
+        // return (compensate + distance / total_distance_);
+        return (compensate + distance / total_distance_);
 }
 
 int AdAStarPlanner::isInList(int ind){
@@ -326,6 +299,59 @@ int AdAStarPlanner::orientationFilter(double th){
     }
 	
 	return th_filter;
+}
+
+std::pair<int, int> AdAStarPlanner::previousPos(int cur_th_filter){
+   std::pair<int, int> det_pos;
+   if(cur_th_filter == 135){
+     det_pos.first = 1;
+     det_pos.second = -1;
+   }
+   if(cur_th_filter == -135){
+     det_pos.first = 1;
+     det_pos.second = 1;
+     return det_pos;
+   }
+   if(cur_th_filter == -90){
+     det_pos.first = 0;
+     det_pos.second = 1;
+     return det_pos;
+   }
+   if(cur_th_filter == 45){
+     det_pos.first = -1;
+     det_pos.second = -1;
+     return det_pos;
+   }
+   if(cur_th_filter == -45){
+     det_pos.first = -1;
+     det_pos.second = 1;
+     return det_pos;
+   }
+   if(cur_th_filter == 0){
+     det_pos.first = -1;
+     det_pos.second = 0;
+     return det_pos;
+   }
+   if(cur_th_filter == 90){
+     det_pos.first = 0;
+     det_pos.second = -1;
+     return det_pos;
+   }
+   if(cur_th_filter == 180){
+     det_pos.first = 1;
+     det_pos.second = 0;
+     return det_pos;
+   }
+   if(cur_th_filter == -180){
+     det_pos.first = 1;
+     det_pos.second = 0;
+     return det_pos;
+   }
+	
+   det_pos.first = 0;
+   det_pos.second = 0;
+	
+   return det_pos;
 }
 
 std::pair<int, int> AdAStarPlanner::nextPos(int cur_th_filter){
