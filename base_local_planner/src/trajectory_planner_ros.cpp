52b82a9cc25293755ede2f68b44ca473713f5b69
wz_hust <wangzhao9562@gmail.com>
2019-12-16 17:26:53 +0800

    Add base_local_planner source file

--- base_local_planner/src/trajectory_planner_ros.cpp
+++ base_local_planner/src/trajectory_planner_ros.cpp
@@ -93,10 +93,18 @@ namespace base_local_planner {
       g_plan_pub_ = private_nh.advertise<nav_msgs::Path>("global_plan", 1);
       l_plan_pub_ = private_nh.advertise<nav_msgs::Path>("local_plan", 1);
 
+
       // test
       test_wp_index_ = private_nh.advertise<std_msgs::String>("wp_index", 1);
       test_goal_ = private_nh.advertise<std_msgs::String>("goal_test", 1);
       test_robot_pos_ = private_nh.advertise<std_msgs::String>("robot_pose_test", 1);
+     
+      // rviz test
+      test_mid_pub_ = private_nh.advertise<visualization_msgs::Marker>("mid_way", 1);
+      test_vis_left_pub_ = private_nh.advertise<visualization_msgs::Marker>("vis_left_way", 1);
+      test_vis_right_pub_ = private_nh.advertise<visualization_msgs::Marker>("vis_right_way", 1);
+      test_obs_detect_left_pub_ = private_nh.advertise<visualization_msgs::Marker>("obs_detect_left_way", 1);
+      test_obs_detect_right_pub_ = private_nh.advertise<visualization_msgs::Marker>("obs_detect_right_way", 1);
 
       tf_ = tf;
       costmap_ros_ = costmap_ros;
@@ -227,6 +235,17 @@ namespace base_local_planner {
       private_nh.param("point_grid/max_obstacle_height", max_obstacle_height, 2.0);
       private_nh.param("point_grid/grid_resolution", grid_resolution, 0.2);
 
+      //parameters for three path avoidance algorithm
+      double follow_vel, obs_detect_dist;
+      double ang_for_left, ang_for_right;
+      double vis_ang_for_left, vis_ang_for_right;
+      private_nh.param("follow_vel", follow_vel, 0.5);
+      private_nh.param("obs_detect_dist", obs_detect_dist, 1.0);
+      private_nh.param("ang_for_left", ang_for_left, 45.0);
+      private_nh.param("ang_for_right", ang_for_right, 45.0);
+      private_nh.param("vis_ang_for_left", vis_ang_for_left, 5.0);
+      private_nh.param("vis_ang_for_right", vis_ang_for_right, 5.0);
+
       ROS_ASSERT_MSG(world_model_type == "costmap", "At this time, only costmap world models are supported by this controller");
       world_model_ = new CostmapModel(*costmap_);
       std::vector<double> y_vels = loadYVels(private_nh);
@@ -237,7 +256,9 @@ namespace base_local_planner {
           acc_lim_x_, acc_lim_y_, acc_lim_theta_, sim_time, sim_granularity, vx_samples, vtheta_samples, pdist_scale,
           gdist_scale, occdist_scale, heading_lookahead, oscillation_reset_dist, escape_reset_dist, escape_reset_theta, holonomic_robot,
           max_vel_x, min_vel_x, max_vel_th_, min_vel_th_, min_in_place_vel_th_, backup_vel,
-          dwa, heading_scoring, heading_scoring_timestep, meter_scoring, simple_attractor, y_vels, stop_time_buffer, sim_period_, angular_sim_granularity);
+          dwa, heading_scoring, heading_scoring_timestep, meter_scoring, simple_attractor, y_vels, stop_time_buffer, sim_period_, angular_sim_granularity,
+          follow_vel, ang_for_left, ang_for_right, obs_detect_dist, 
+          vis_ang_for_left, vis_ang_for_right);
 
       map_viz_.initialize(name, global_frame_, boost::bind(&TrajectoryPlanner::getCellCosts, tc_, _1, _2, _3, _4, _5, _6));
       initialized_ = true;
@@ -375,77 +396,256 @@ namespace base_local_planner {
     return true;
   }
   
-  // 跟随状态下速度指令计算
-  bool TrajectoryPlannerROS::computeVelocityCommandsInStateFollowing(geometry_msgs::Twist& cmd_vel, unsigned int& following_wp_num){
+  bool TrajectoryPlannerROS::isFollowBlocked(unsigned int following_wp_num){
+    ROS_INFO("Check before avoiding");
+    if (! isInitialized()){
+      ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+      return false;
+    }
+    ROS_INFO("bad_alloc: try get robot pose");
+    tf::Stamped<tf::Pose> global_pose;
+    if(!costmap_ros_->getRobotPose(global_pose)){
+        return false;
+    }
+    ROS_INFO("bad_alloc: try transform pose to global frame");
+    std::vector<geometry_msgs::PoseStamped> transformed_plan;
+    if(!transformGlobalPlan(*tf_, global_plan_, global_pose, *costmap_, global_frame_, transformed_plan)){
+      ROS_WARN("Could not transform the global plan to the frame of the controller");
+      return false;
+    }
+    ROS_INFO("bad_alloc: test is the transformed plan empty");
+    //if the global plan passed in is empty... we won't do anything
+    if(transformed_plan.empty()){
+      ROS_WARN("move_base: global plan is empty!");
+      return false;
+    }
+
+    tf::Stamped<tf::Pose> goal_point;
+
+    ROS_INFO("bad_alloc: try transform pose to tf msg from pose msg");
+
+    try{
+      tf::poseStampedMsgToTF(transformed_plan[following_wp_num], goal_point);
+    }
+    catch(std::exception& e){
+      ROS_ERROR("bad_alloc: transfer pose to tf msg from pose msg failed");
+    }
+    //we assume the global goal is the last point in the global plan
+    double goal_x = goal_point.getOrigin().getX();
+    double goal_y = goal_point.getOrigin().getY();
+    // double yaw = tf::getYaw(goal_point.getRotation());
+    // double goal_th = yaw;
+
+
+    // get current position of robot
+    double pos_x = global_pose.getOrigin().getX();
+    double pos_y = global_pose.getOrigin().getY();
+    double pos_th = tf::getYaw(global_pose.getRotation());
+    // double pos_th = global_pose.getRotation();
+
+    ROS_INFO("bad_alloc: try publish rviz visualization parameters");
+    /*
+    // rviz test(abandon)
+    double mid_x, mid_y, vis_left_x, vis_left_y, vis_right_x, vis_right_y;
+    // visual area
+    tc_->getVisualArea(pos_x, pos_y, pos_th, mid_x, mid_y, vis_left_x, vis_left_y, vis_right_x, vis_right_y);
+    visualization_msgs::Marker mid_line, vis_left_line, vis_right_line;
+    getLineVisMsgs(pos_x, pos_y, mid_x, mid_y, mid_line, VisAreaType::VISUALAREA);
+    test_mid_pub_.publish(mid_line);
+    getLineVisMsgs(pos_x, pos_y, vis_left_x, vis_left_y, vis_left_line, VisAreaType::VISUALAREA);
+    test_vis_left_pub_.publish(vis_left_line);
+    getLineVisMsgs(pos_x, pos_y, vis_right_x, vis_right_y, vis_right_line, VisAreaType::VISUALAREA);
+    test_vis_right_pub_.publish(vis_right_line);
+   
+    // rviz test
+    double mid_obs_x, mid_obs_y, left_x, left_y, right_x, right_y;
+    // obstacle detection area
+    tc_->getObsDetectionArea(pos_x, pos_y, pos_th, mid_obs_x, mid_obs_y, left_x, left_y, right_x, right_y);
+    visualization_msgs::Marker mid_obs_line, obs_left_line, obs_right_line;
+    getLineVisMsgs(pos_x, pos_y, left_x, left_y, obs_left_line, VisAreaType::OBSDETECTAREA);
+    test_obs_detect_left_pub_.publish(obs_left_line);
+    getLineVisMsgs(pos_x, pos_y, right_x, right_y, obs_right_line, VisAreaType::OBSDETECTAREA);
+    test_obs_detect_right_pub_.publish(obs_right_line);
+    */
+    // rviz test
+    geometry_msgs::PoseStamped mid, vis_left, vis_right;
+    std::vector<geometry_msgs::PoseStamped> vis_vec = tc_->getVisualArea(pos_x, pos_y, pos_th);
+    vis_vec[0].header.frame_id = global_frame_;
+    vis_vec[1].header.frame_id = global_frame_;
+    vis_vec[2].header.frame_id = global_frame_;
+    tf_->transformPose("odom", vis_vec[0], mid); // world frame to base of robot
+    tf_->transformPose("odom", vis_vec[1], vis_left);
+    tf_->transformPose("odom", vis_vec[2], vis_right);
+    visualization_msgs::Marker mid_line, vis_left_line, vis_right_line;
+    getLineVisMsgs(pos_x, pos_y, mid, mid_line, VisAreaType::VISUALAREA);
+    getLineVisMsgs(pos_x, pos_y, vis_left, vis_left_line, VisAreaType::VISUALAREA);
+    getLineVisMsgs(pos_x, pos_y, vis_right, vis_right_line, VisAreaType::VISUALAREA);
+    test_mid_pub_.publish(mid_line);
+    test_vis_left_pub_.publish(vis_left_line);
+    test_vis_right_pub_.publish(vis_right_line);
+
+    // rviz test
+    geometry_msgs::PoseStamped obs_left, obs_right;
+    std::vector<geometry_msgs::PoseStamped> obs_vec = tc_->getObsDetectionArea(pos_x, pos_y, pos_th);
+    obs_vec[0].header.frame_id = global_frame_;
+    obs_vec[1].header.frame_id = global_frame_;
+    obs_vec[2].header.frame_id = global_frame_;
+    tf_->transformPose("odom", obs_vec[1], obs_left);
+    tf_->transformPose("odom", obs_vec[2], obs_right);
+    visualization_msgs::Marker obs_left_line, obs_right_line;
+    getLineVisMsgs(pos_x, pos_y, obs_left, obs_left_line, VisAreaType::OBSDETECTAREA);
+    getLineVisMsgs(pos_x, pos_y, obs_right, obs_right_line, VisAreaType::OBSDETECTAREA);
+    test_obs_detect_left_pub_.publish(obs_left_line);
+    test_obs_detect_right_pub_.publish(obs_right_line);
+    
+    ROS_INFO("bad_alloc: is follow blocked");
+    if(!tc_->isFollowBlocked(goal_x, goal_y, pos_x, pos_y, pos_th)){
+      // ROS_WARN("move_base: no block in followed way");
+      return false;
+    }
+    // ROS_WARN("move_base: blocked in followed way");
+    return true;
+  }
+ 
+
+  bool TrajectoryPlannerROS::isStopAvoidance(){
+    if (! isInitialized()){
+      ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+      return false;
+    }
+    tf::Stamped<tf::Pose> global_pose;
+    if(!costmap_ros_->getRobotPose(global_pose)){
+        return false;
+    }
+
+    // get current position of robot
+    double pos_x = global_pose.getOrigin().getX();
+    double pos_y = global_pose.getOrigin().getY();
+    double pos_th = tf::getYaw(global_pose.getRotation());
+
+    if(tc_->isFrontPathFree(pos_x, pos_y, pos_th)){
+      // ROS_WARN("move_base: free in three path"); 
+      return true;
+    }
+    // ROS_WARN("move_base: block in three path");
+    return false;
+  }
+
+  bool TrajectoryPlannerROS::computeVelCommandsInStateOfAvoid(geometry_msgs::Twist& cmd_vel, unsigned int& following_wp_num){
+    ROS_INFO("Check before avoiding");
+    if (! isInitialized()) {
+      ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+      return false;
+    }
+    tf::Stamped<tf::Pose> global_pose;
+    if(!costmap_ros_->getRobotPose(global_pose)){
+        return false;
+    }
+
+    // get current position of robot
+    double pos_x = global_pose.getOrigin().getX();
+    double pos_y = global_pose.getOrigin().getY();
+    double pos_th = tf::getYaw(global_pose.getRotation());
+
+    if(tc_->pointFollowWithTPMethod(pos_x, pos_y, pos_th, cmd_vel.linear.x, cmd_vel.linear.y, cmd_vel.angular.z)){
+      following_wp_num = 1;
+    }
+    else{
+      return false;
+    }
+    return true;
+  }
+
+  bool TrajectoryPlannerROS::computeVelCommandsInStateOfAvoid(geometry_msgs::PoseStamped& next_goal, unsigned int& following_wp_num){
+    ROS_INFO("Check before avoiding");
+    if (! isInitialized()) {
+      ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+      return false;
+    }
+    tf::Stamped<tf::Pose> global_pose;
+    if(!costmap_ros_->getRobotPose(global_pose)){
+        return false;
+    }
+
+    // get current position of robot
+    double pos_x = global_pose.getOrigin().getX();
+    double pos_y = global_pose.getOrigin().getY();
+    double pos_th = tf::getYaw(global_pose.getRotation());
+
+    if(tc_->pointFollowWithTPMethod(pos_x, pos_y, pos_th, next_goal.pose.position.x, next_goal.pose.position.y)){
+      following_wp_num = 1;
+    }
+    else{
+      return false;
+    }
+    return true;
+  }
+  
+  bool TrajectoryPlannerROS::computeVelCommandsInStateOfFollow(geometry_msgs::Twist& cmd_vel, unsigned int& following_wp_num){
     ROS_INFO("Checking before follow");	
     if (! isInitialized()) {
       ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
       return false;
     }
-	tf::Stamped<tf::Pose> global_pose;
-	if(!costmap_ros_->getRobotPose(global_pose)){
-		return false;
-	}
-	
-	std::vector<geometry_msgs::PoseStamped> transformed_plan;
-	if(!transformGlobalPlan(*tf_, global_plan_, global_pose, *costmap_, global_frame_, transformed_plan)){
-	  ROS_WARN("Could not transform the global plan to the frame of the controller");
+    tf::Stamped<tf::Pose> global_pose;
+    if(!costmap_ros_->getRobotPose(global_pose)){
       return false;
-	}
+    }
 	
-	    //if the global plan passed in is empty... we won't do anything
+    std::vector<geometry_msgs::PoseStamped> transformed_plan;
+    if(!transformGlobalPlan(*tf_, global_plan_, global_pose, *costmap_, global_frame_, transformed_plan)){
+      ROS_WARN("Could not transform the global plan to the frame of the controller");
+      return false;
+    }
+
+    //if the global plan passed in is empty... we won't do anything
     if(transformed_plan.empty())
       return false;
 
     tf::Stamped<tf::Pose> goal_point;
 
-    ROS_INFO("get goal msg");	
     tf::poseStampedMsgToTF(transformed_plan[following_wp_num], goal_point);
-    ROS_INFO("transform goal msg");
     //we assume the global goal is the last point in the global plan
     double goal_x = goal_point.getOrigin().getX();
     double goal_y = goal_point.getOrigin().getY();
     double yaw = tf::getYaw(goal_point.getRotation());
     double goal_th = yaw;
-    
+   
+     
     // test
     std_msgs::String goal_test_msgs;
     std::stringstream str_goal_test;
     str_goal_test << goal_x << "," << goal_y << "," << goal_th;
     goal_test_msgs.data = str_goal_test.str();
     test_goal_.publish(goal_test_msgs);
+    
+    double pos_x = global_pose.getOrigin().getX();
+    double pos_y = global_pose.getOrigin().getY();
+    double pos_th = tf::getYaw(global_pose.getRotation());
+    
+    // test
+    std_msgs::String pose_test_msgs;
+    std::stringstream str_pose_test;
+    str_pose_test << pos_x << "," << pos_y << "," << pos_th;
+    pose_test_msgs.data = str_pose_test.str();
+    test_robot_pos_.publish(pose_test_msgs);
 
-        
-	// 获取当前机器人位置
-        ROS_INFO("get robot position");
-	double pos_x = global_pose.getOrigin().getX();
-	double pos_y = global_pose.getOrigin().getY();
-	double pos_th = tf::getYaw(global_pose.getRotation());
-
-        // test
-        std_msgs::String pose_test_msgs;
-        std::stringstream str_pose_test;
-        str_pose_test << pos_x << "," << pos_y << "," << pos_th;
-        pose_test_msgs.data = str_pose_test.str();
-        test_robot_pos_.publish(pose_test_msgs);
-
-        // test
-        ROS_WARN("State: FOLLOWING");
-        std_msgs::String num;
-        std::stringstream str_num;
-        str_num << following_wp_num;
-        num.data = str_num.str();
-        test_wp_index_.publish(num);
+    // test
+    std_msgs::String num;
+    std::stringstream str_num;
+    str_num << following_wp_num;
+    num.data = str_num.str();
+    test_wp_index_.publish(num);
 	
-	if(tc_->pointFollow(goal_x, goal_y, goal_th, pos_x, pos_y, pos_th, cmd_vel.linear.x, cmd_vel.linear.y, cmd_vel.angular.z))
-		following_wp_num++;
+    if(tc_->pointFollow(goal_x, goal_y, goal_th, pos_x, pos_y, pos_th, cmd_vel.linear.x, cmd_vel.linear.y, cmd_vel.angular.z))
+      following_wp_num++;
 	
-	if(following_wp_num == transformed_plan.size() - 1){
-		reached_goal_ = true; // 到达规划路径最后目标点
-                following_wp_num = 0;
-        }
+    if(following_wp_num == transformed_plan.size() - 1){
+      reached_goal_ = true; 
+      following_wp_num = 1;
+    }
 
-	return true;
+    return true;
   }
   
 
@@ -674,5 +874,39 @@ namespace base_local_planner {
     }
     //return flag set in controller
     return reached_goal_; 
+  }
+
+  // rvis test interface
+  void TrajectoryPlannerROS::getLineVisMsgs(double sp_x, double sp_y, double ep_x, double ep_y, visualization_msgs::Marker& line_msgs, VisAreaType vis_area_type){
+    line_msgs.header.frame_id = "map";
+    line_msgs.header.stamp = ros::Time::now();
+    line_msgs.ns = "lines";
+    line_msgs.action = visualization_msgs::Marker::ADD;
+    line_msgs.type = visualization_msgs::Marker::LINE_LIST;
+    line_msgs.scale.x = 0.01;
+    if(vis_area_type == VisAreaType::VISUALAREA){
+      line_msgs.color.b = 1.0;
+    }
+    else{
+      line_msgs.color.r = 1.0;
+    }
+    line_msgs.color.a = 1.0;
+    geometry_msgs::Point s_p;
+    s_p.x = sp_x;
+    s_p.y = sp_y;
+    s_p.z = 0;
+    geometry_msgs::Point e_p;
+    e_p.x = ep_x;
+    e_p.y = ep_y;
+    e_p.z = 0;
+    line_msgs.points.push_back(s_p);
+    line_msgs.points.push_back(e_p);
+  }
+
+  // rviz test interface
+  void TrajectoryPlannerROS::getLineVisMsgs(double sp_x, double sp_y, geometry_msgs::PoseStamped e_pos, visualization_msgs::Marker& line_msgs, VisAreaType vis_area_type){
+    double ep_x = e_pos.pose.position.x;
+    double ep_y = e_pos.pose.position.y;
+    getLineVisMsgs(sp_x, sp_y, ep_x, ep_y, line_msgs, vis_area_type); 
   }
 };
