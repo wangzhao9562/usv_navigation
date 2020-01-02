345c38ab5da685a2d4af7884867a4e6dfbcbde4c
wangzhao9562 <wangzhao9562@gmail.com>
2020-01-02 19:36:15 +0800

    Submit complete map_loader implementation

--- map_server/src/map_loader.cpp
+++ map_server/src/map_loader.cpp
@@ -49,6 +49,8 @@ namespace map_server{
         map_sub_ = private_nh.subscribe("map_info", 1, &MapLoader::mapCallback, this);
         map_pub_ = private_nh.advertise<nav_msgs::OccupancyGrid>("map_info", 1, true);
         map_pub_ = private_nh.advertise<std_msgs::String>("map_path", 1, true);
+
+	boost::thread map_recv(boost::bind(&MapLoader::mapTransform, this)); // create thread to listen tcp port
     };
 
     MapLoader::~MapLoader(){
@@ -80,6 +82,16 @@ namespace map_server{
         for(unsigned int y = 0; y < map->info.height; y++) {
             for(unsigned int x = 0; x < map->info.width; x++) {
                 unsigned int i = x + (map->info.height - y - 1) * map->info.width;
+		if(map->data[i] == 0){
+			fputc(254, out);
+		}
+		else if(map->data[i] == 255){
+			fputc(000, out);
+		}
+		else{
+			fputc(205, out);
+		}
+		/*
                 if (map->data[i] >= 0 && map->data[i] <= threshold_free_) { //occ [0,0.1)
                     fputc(254, out);
                 } else if (map->data[i] <= 100 && map->data[i] >= threshold_occupied_) { //occ (0.65,1]
@@ -87,12 +99,13 @@ namespace map_server{
                 } else { //occ [0.1,0.65]
                     fputc(205, out);
                 }
+		*/
             }
         }
 
         fclose(out);
 
-        std::string mapmetadatafile = mapname_ + ".yaml";
+        std::string mapmetadatafile = map_name_ + ".yaml";
         ROS_INFO("Writing map occupancy data to %s", mapmetadatafile.c_str());
         FILE* yaml = fopen(mapmetadatafile.c_str(), "w");
 
@@ -123,56 +136,79 @@ namespace map_server{
     };
 
     void MapLoader::mapTransform(){
-        /* Receive map info from workstation through boost TCP socket */
-        std::vector<u_int8_t> map_info_buf;
-        tcp_c_->getBuf(map_info_buf);
-        /* Decode message through mavlink protocol */
-        mavlink_message_t mav_msg; // mavlink message
-        mavlink_status_t mav_status; // mavlink status
-        // mavlink_map_info_t mav_map_info;
+	while(ros::ok()){
+		/* Receive map info from workstation through boost TCP socket */
+		std::vector<u_int8_t> map_info_buf;
+		tcp_c_->getBuf(map_info_buf);
+		/* Decode message through mavlink protocol */
+		mavlink_message_t mav_msg; // mavlink message
+		mavlink_status_t mav_status; // mavlink status
+		// mavlink_map_info_t mav_map_info;
 
-        std::string map_name; 
-        u_int32_t map_size;
-        std::vector<u_int16_t> map_grid_info;
-        double map_resolution;
-        std::pair<u_int32_t, u_int32_t> map_origin;
-        std::pair<u_int32_t, u_int32_t> map_coord;
+		std::string tp_map_name;
+		u_int32_t map_width, map_height;
+		std::vector<u_int16_t> map_grid_info;
+		double map_resolution;
+		std::pair<u_int32_t, u_int32_t> map_origin;
+		std::pair<u_int32_t, u_int32_t> map_coord;
 
-        // decode
-        for(int ind = 0; ind < map_info_buf.size(); ++ind){
-            u_int8_t mav_c = map_info_buf[ind];
-            if(mavlink_parse_char(MAVLINK_COMM_0, mav_c, &mav_msg, &mav_status)){
-                switch(mav_msg.msgid){
-                    case MAP_INFO:
-                    {
-                        // get map name 
-                        u_int8_t* tp_map_name;
-                        mavlink_msg_map_info_get_map_name(&mav_msg, tp_map_name);
-                        map_name = std::string(tp_map_name);
-                        
-                        // get map grid information
-                        u_int_32_t map_width, map_height;
-                        map_width = mavlink_msg_map_info_get_map_width(&mav_msg);
-                        map_height = mavlink_msg_map_info_get_map_height(&mav_msg);
-                        map_size = map_width * map_height;
-                        u_int_16_t tp_map_grid_info[map_size];
-                        mavlink_msg_map_info_get_occupancy_grid(&mav_msg, tp_map_grid_info);
-                        map_grid_info.insert(map_grid_info.end(), tp_map_grid_info, tp_map_grid_info + map_size);
+		// decode
+		for(int ind = 0; ind < map_info_buf.size(); ++ind){
+		    u_int8_t mav_c = map_info_buf[ind];
+		    if(mavlink_parse_char(MAVLINK_COMM_0, mav_c, &mav_msg, &mav_status)){
+			switch(mav_msg.msgid){
+			    case MAP_INFO:
+			    {
+				// get map name 
+				u_int8_t* tp_map_name;
+				mavlink_msg_map_info_get_map_name(&mav_msg, tp_map_name);
+				tp_map_name = std::string(tp_map_name);
+				
+				// get map grid information
+				map_width = mavlink_msg_map_info_get_map_width(&mav_msg);
+				map_height = mavlink_msg_map_info_get_map_height(&mav_msg);
+				u_int32_t map_size = map_width * map_height;
+				u_int_16_t tp_map_grid_info[map_size];
+				mavlink_msg_map_info_get_occupancy_grid(&mav_msg, tp_map_grid_info);
+				map_grid_info.insert(map_grid_info.end(), tp_map_grid_info, tp_map_grid_info + map_size);
 
-                        // get map resolution
-                        map_resolution = mavlink_msg_map_info_get_resolution(&map_msg);
+				// get map resolution
+				map_resolution = mavlink_msg_map_info_get_resolution(&map_msg);
 
-                        // get map origin point
-                        map_origin.first = mavlink_msg_map_info_get_origin_x(&mav_msg);
-                        map_origin.second = mavlink_msg_map_info_get_origin_y(&mav_msg);
+				// get map origin point
+				map_origin.first = mavlink_msg_map_info_get_origin_x(&mav_msg);
+				map_origin.second = mavlink_msg_map_info_get_origin_y(&mav_msg);
 
-                        // get map coordinate in last map
-                        map_coord.first = mavlink_msg_map_info_get_x_in_last_map(&mav_msg);
-                        map_coord.second = mavlink_msg_map_info_get_y_in_last_map(&mav_msg);
-                    }
-                }
-            }
-        }
-        /* Transform map info into formation of nav_msgs::OccupancyGrid and publish it on innter topic*/
+				// get map coordinate in last map
+				map_coord.first = mavlink_msg_map_info_get_x_in_last_map(&mav_msg);
+				map_coord.second = mavlink_msg_map_info_get_y_in_last_map(&mav_msg);
+				break; 
+			    }
+			    default:
+				break;
+			}
+		    }
+		}
+		/* Transform map info into formation of nav_msgs::OccupancyGrid and publish it on innter topic*/
+		if(map_name != tp_map_name){
+			map_name_ = tp_map_name;
+
+			nav_msgs::OccupancyGrid map_info_msg;
+			map_info_msg.header.frame_id = "map";
+			map_info_msg.header.stamp = ros::Time::now();
+
+			map_info_msg.info.resolution = map_resolution;
+			map_info_msg.info.width = map_width;
+			map_info_msg.info.height = map_height;
+			map_info_msg.info.origin.position.x = map_origin.first;
+			map_info_msg.info.origin.position.y = map_origin.second;
+			map_info_msg.info.origin.orientation.w = 1.0;	
+
+			map_info_msg.data = map_info_buf; // not sure
+
+			map_info_pub_.publish(map_info_buf); // publish
+		}
+		boost::this_thread.sleep(boost::posix_time::second(1));
+	}
     };
 }; // end of namespace
