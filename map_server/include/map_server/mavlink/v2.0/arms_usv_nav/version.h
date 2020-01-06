ade9f09a106b0e54268468cebd3d0f970a88a949
wangzhao9562 <wangzhao9562@gmail.com>
2020-01-06 22:20:17 +0800

    Update ARMsUsvNav mavlink message

---
+++ map_server/include/map_server/mavlink/v2.0/arms_usv_nav/version.h
@@ -0,0 +1,14 @@
+/** @file
+ *  @brief MAVLink comm protocol built from arms_usv_nav.xml
+ *  @see http://mavlink.org
+ */
+#pragma once
+ 
+#ifndef MAVLINK_VERSION_H
+#define MAVLINK_VERSION_H
+
+#define MAVLINK_BUILD_DATE "Mon Jan 06 2020"
+#define MAVLINK_WIRE_PROTOCOL_VERSION "2.0"
+#define MAVLINK_MAX_DIALECT_PAYLOAD_SIZE 8294688
+ 
+#endif // MAVLINK_VERSION_H
